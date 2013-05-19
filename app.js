cb13f743886f16926f56d678571458468dab5e85
Ev Bogue <ev@evbogue.com>
2013-05-19 16:29:02 -0400

    remove old shit route

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
-app.get('/oldshit', routes.oldshit);
 app.get('/writing', routes.writing);
 app.get('/node', routes.node);
 app.get('/nodejs', routes.node);
