ceb7348e9b943bcc042b274208337eb02f62d0e6
Ev Bogue <ev@evbogue.com>
2013-05-19 16:28:55 -0400

    remove old shit route

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,6 @@ exports.about = function(req, res){
   res.render('about', { title: 'About | Ev Bogue'});
 };
 
-exports.oldshit = function(req, res){
-  res.render('oldshit', { title: 'Buy my old shit | Ev Bogue'});
-};
-
 exports.writing = function(req, res){
   res.render('writing', { title: 'Writing | Ev Bogue'});
 };
